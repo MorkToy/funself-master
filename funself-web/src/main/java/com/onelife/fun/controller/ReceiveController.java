d0fc8a728bcc4f307a96e7d685663a8b5ca3bcad
xiqian <291318254@qq.com>
2016-11-21 10:31:24 +0800

    nothing

--- funself-web/src/main/java/com/onelife/fun/controller/ReceiveController.java
+++ funself-web/src/main/java/com/onelife/fun/controller/ReceiveController.java
@@ -1,13 +1,7 @@
 package com.onelife.fun.controller;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by wudi.
